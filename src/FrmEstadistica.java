c128405b0c7bb662e8338bf7a40aecddb643a03d
fray osorio <frayosorio@gmail.com>
2025-02-05 11:47:18 -0500

    Mostrar los datos en un Jlist

--- src/FrmEstadistica.java
+++ src/FrmEstadistica.java
@@ -3,8 +3,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-
 public class FrmEstadistica extends JFrame {
+
+    JTextField txtDato;
+    JList lstMuestra;
 
     public FrmEstadistica() {
 
@@ -13,51 +15,65 @@ public class FrmEstadistica extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        JLabel lblDato=new JLabel("Dato");
-        lblDato.setBounds(10,10,100,25);
+        JLabel lblDato = new JLabel("Dato");
+        lblDato.setBounds(10, 10, 100, 25);
         getContentPane().add(lblDato);
 
-        JTextField txtDato=new JTextField();
-        txtDato.setBounds(80,10,100,25);
+        txtDato = new JTextField();
+        txtDato.setBounds(80, 10, 100, 25);
         getContentPane().add(txtDato);
 
-        JLabel lblDatos=new JLabel("Muestra");
-        lblDatos.setBounds(210,10,100,25);
+        JLabel lblDatos = new JLabel("Muestra");
+        lblDatos.setBounds(210, 10, 100, 25);
         getContentPane().add(lblDatos);
 
-        JButton btnAgregar=new JButton("Agregar");
-        btnAgregar.setBounds(80,40,100,25);
+        JButton btnAgregar = new JButton("Agregar");
+        btnAgregar.setBounds(80, 40, 100, 25);
         getContentPane().add(btnAgregar);
 
-        JButton btnQuitar=new JButton("Quitar");
-        btnQuitar.setBounds(80,70,100,25);
+        JButton btnQuitar = new JButton("Quitar");
+        btnQuitar.setBounds(80, 70, 100, 25);
         getContentPane().add(btnQuitar);
 
-        JList lstMuestra=new JList();
+        lstMuestra = new JList();
         lstMuestra.setBounds(210, 40, 100, 150);
         getContentPane().add(lstMuestra);
 
-        //eventos de la GUI
-        btnAgregar.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent ae){
+        // eventos de la GUI
+        btnAgregar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
                 agregarDato();
             }
         });
 
-        btnQuitar.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent ae){
+        btnQuitar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
                 quitarDato();
             }
         });
     }
 
+    // declarar el arreglo que almacenará los datos de la muestra
+    private double[] muestra = new double[1000];
+    private int totalDatos = -1;
 
-    private void agregarDato(){
-
+    private void agregarDato() {
+        double dato = Double.parseDouble(txtDato.getText());
+        totalDatos++;
+        muestra[totalDatos] = dato;
+        mostrarMuestra();
     }
 
-    private void quitarDato(){
+    private void mostrarMuestra() {
+        String[] strMuestra = new String[totalDatos + 1];
+        for (int i = 0; i <= totalDatos; i++) {
+            strMuestra[i] = String.valueOf(muestra[i]);
+        }
+        lstMuestra.setListData(strMuestra);
+    }
 
+    private void quitarDato() {
+        JOptionPane.showMessageDialog(null, "Hizo clic en QUITAR");
     }
 
 }
